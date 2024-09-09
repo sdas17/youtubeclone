11131e7c01eda925b625ab19dfac6f7e7409ed18
sdas1 <sdas1@miraclesoft.com>
2024-09-09 15:36:41 +0530

    namasteyoutubechanngel

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # youtubeclone
 youtube clone akshay saini
+
+# how to setup tailwind
+npm i -D tailwindcss
+npx tailwindcss init
+
+tell what file track
+"./src/**/*.js"
+
+tailwind base,component,utilities
---
+++ youtube-clne/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Head from './Head';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+
+      {/* *******
+      ***
+      **
+      Head
+      Body
+        sidebar
+         Menuitems
+      MainCotainer
+       Buttonslist
+       videContainer
+       Videocard
+      
+      
+      
+      
+      */}
+      <Head/>
+    </div>
+  );
+}
+
+export default App;
